80e0b3392e27d1cc44db43bf4755a40504ada57a
deemson <deemson.k@gmail.com>
2026-04-16 15:58:33 +0200

    go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/deemson/gbx
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Checking out what's the first commit like, huh? Well, stay awesome.")
+}
